78dcb8fcb1367c275e209b5ba096840b6c743c9a
unknown <tcutlip08@gmail.com>
2020-01-14 17:14:52 -0500

    got check boxes working. need to fix the set up of state still

--- client/src/containers/Home.js
+++ client/src/containers/Home.js
@@ -5,12 +5,18 @@ import axios from "axios";
 
 class Home extends Component {
   state = {
-    hunter: true,
-    mage: true,
-    assassin: true,
-    warrior: true,
-    guardian: true
+    gods: [
+      { class: "Guardian", checked: true },
+      { class: "Warrior", checked: true },
+      { class: "Assassin", checked: true },
+      { class: "Mage", checked: true },
+      { class: "Hunter", checked: true }
+    ]
   };
+
+  componentDidUpdate() {
+    console.log(this.state);
+  }
 
   randomize() {
     axios
@@ -22,6 +28,13 @@ class Home extends Component {
         console.log(err);
       });
   }
+
+  changeCheckBox = event => {
+    const { name, checked } = event.target;
+    this.setState({
+      [name]: checked
+    });
+  };
   render() {
     return (
       <div className="container">
@@ -43,6 +56,61 @@ class Home extends Component {
             </button>
           </div>
           <div className="col"></div>
+        </div>
+        <div className="row">
+          <div className="col">
+            <input
+              type="checkbox"
+              name="guardian"
+              defaultChecked={this.state.gods[0].checked}
+              onChange={this.changeCheckBox}
+            />
+            <span className="class">Guardian</span>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col">
+            <input
+              type="checkbox"
+              name="warrior"
+              defaultChecked={this.state.gods[1].checked}
+              onChange={this.changeCheckBox}
+            />
+            <span className="class">Warrior</span>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col">
+            <input
+              type="checkbox"
+              name="assassin"
+              defaultChecked={this.state.gods[2].checked}
+              onChange={this.changeCheckBox}
+            />
+            <span className="class">Assassin</span>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col">
+            <input
+              type="checkbox"
+              name="mage"
+              defaultChecked={this.state.gods[3].checked}
+              onChange={this.changeCheckBox}
+            />
+            <span className="class">Mage</span>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col">
+            <input
+              type="checkbox"
+              name="hunter"
+              defaultChecked={this.state.gods[4].checked}
+              onChange={this.changeCheckBox}
+            />
+            <span className="class">Hunter</span>
+          </div>
         </div>
       </div>
     );
